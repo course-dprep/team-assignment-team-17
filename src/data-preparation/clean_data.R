f5d6c4335d7f7a8f0501a98e5c2d3b4c8dffd344
JanneDonkers <112557841+JanneDonkers@users.noreply.github.com>
2022-10-06 10:29:32 +0200

    Update clean_data.R

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,11 +1,61 @@
-# Load merged data 
-load("./gen/data-preparation/temp/data_merged.RData")
+#Duplicates 
 
-# Drop observations with V1 <= -0.9
-df_cleaned <- df_merged[df_merged$V1 > -0.9,]
+#Count the number of unique id's
+#Count the number of unique listing_id's in the calendar dataset and the number of unique id's in the listings dataset 
 
-# Remove V1
-df_cleaned <- df_cleaned[,c(1,2,4:7)]
+calendar_unique <- unique(calendar$listing_id)
+length(calendar_unique)
 
-# Save cleaned data
-save(df_cleaned,file="./gen/data-preparation/output/data_cleaned.RData")
+listings_unique <- unique(listings$id)
+length(listings_unique)
+
+#Count the number of full duplicates in the calendar and listings datset
+
+sum(duplicated(calendar))
+sum(duplicated(listings))
+
+# Dropping columns 
+
+calendar_x <- select(calendar, c("listing_id", "price", "available", "adjusted_price", "date"))
+listing_x <- select(listings, c("id", "neighbourhood_cleansed", "price", "availability_365"))
+
+#Checking data types
+
+is.numeric(listing_x$id)
+is.character(listing_x$neighbourhood_cleansed)
+is.numeric(listing_x$price)
+is.numeric(listing_x$availability_365)
+sapply(calendar_x, class)
+
+#Changing data types
+#Editing
+listing_x$price = str_remove(listing_x$price,",")
+listing_x$price = as.numeric(gsub("\\$", "", listing_x$price))
+
+#Converting
+as.numeric(listing_x$price)
+
+#Checking data type and NA's
+is.numeric(listing_x$price)
+
+sum(is.na(listing_x$price))
+sum(is.na(listings$price))
+
+#Editing
+calendar_x$price = str_remove(calendar_x$price, ",")
+calendar_x$adjusted_price = str_remove(calendar_x$adjusted_price,",")
+
+calendar_x$price = as.numeric(gsub("\\$", "", calendar_x$price))
+calendar_x$adjusted_price = as.numeric(gsub("\\$", "", calendar_x$adjusted_price))
+
+#Converting
+as.numeric(calendar_x$price)
+as.numeric(calendar_x$adjusted_price)
+
+#Checking data type and NA's
+is.numeric(calendar_x$price)
+is.numeric(calendar_x$adjusted_price)
+
+sum(is.na(listing_x$price))
+sum(is.na(calendar_x$price))
+sum(is.na(calendar_x$adjusted_price))
