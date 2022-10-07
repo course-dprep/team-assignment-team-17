a2ed4227a4a95eb6d732444d806e3db158575e76
Noremessing <113368351+Noremessing@users.noreply.github.com>
2022-10-07 11:07:12 +0200

    Update download_data.R

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -6,3 +6,12 @@ download.file('https://rgreminger.github.io/files/dataset1.csv','./data/dataset1
 # dir.create('./data/dataset2')  # Uncomment if need to create directory with R
 download.file('https://rgreminger.github.io/files/dataset2.csv','./data/dataset2/dataset2.csv')
 
+## downloading datasets from insideairbnb.com ##
+# download the listing dataset as .csv.gz file
+http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/data/listings.csv.gz
+#download the calendar dataset as .csv.gz file
+http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/data/calendar.csv.gz
+
+#converting files from .csv.gz to .csv
+gunzip("calendar.csv.gz", remove=FALSE)
+gunzip("listings.csv.gz", remove=FALSE)
